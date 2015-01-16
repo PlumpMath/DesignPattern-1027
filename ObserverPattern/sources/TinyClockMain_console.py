668aab9b257dac7a0fdcd2f3c399ebe78413f4f9
kantoku009 <kantoku009@hotmail.com>
2015-01-16 23:36:30 +0900

    rename main filename

---
+++ ObserverPattern/sources/TinyClockMain_console.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@brief  TinyClock console版.
+"""
+
+import threading
+import time         #time.sleep()を使用する為.
+
+from View.TinyClockView import TinyClockView
+from Model.TinyClockModel import TinyClockModel
+
+def refresh_view(i_View):
+    """
+    @brief  Viewのリフレッシュ.
+            時刻を表示する.
+            時計表示スレッドから呼ばれる.
+    """
+    while 1:
+        #1秒スリープ.
+        time.sleep(1.0)
+        theThreadName = threading.currentThread().getName()
+        print("%s \t %s")%(theThreadName, i_View.get_time())
+
+
+def main():
+    """
+    @brief  TinyClockView console版 main処理
+    @note   以下を別スレッドを生成して時刻を表示する.
+                アナログ時計
+                デジタル時計
+            スリープを使用してスレッドを切り替えている為, 
+            スレッドの同期はとっていない.
+    """
+    #時計本体部を生成.
+    theModel = TinyClockModel()
+    #アナログ時計表示部を生成.
+    theAnalogClockView = TinyClockView()
+    #デジタル時計表示部を生成.
+    theDigitalClockView = TinyClockView()
+
+    #時計 本体部と表示部接続.
+    theModel.attach(theAnalogClockView)
+    theModel.attach(theDigitalClockView)
+
+    #アナログ時計表示部のスレッドを生成.
+    theAnalogClockViewThread= threading.Thread(target=refresh_view, name="AnalogClock", args=[theAnalogClockView])
+    theAnalogClockViewThread.setDaemon(True)
+    theAnalogClockViewThread.start()
+
+    #デジタル時計表示部のスレッドを生成.
+    theDigitalClockViewThread = threading.Thread(target=refresh_view, name="DigitalClock", args=[theDigitalClockView])
+    theDigitalClockViewThread.setDaemon(True)
+    theDigitalClockViewThread.start()
+
+
+    while 1:
+        theModel.set_time()
+        #1秒スリープ.
+        time.sleep(1.0)
+
+if __name__=='__main__':
+	main()
+
---
+++ ObserverPattern/sources/TinyClockMain_gui.py
@@ -0,0 +1,78 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@brief  TinyClock GUI版.
+        PyQtを使用している.
+        TinyClock GUI版を使用するには, PyQtのインストールが必要.
+"""
+
+import sys
+
+#PyQt
+import PyQt4.QtCore as QtCore
+import PyQt4.QtGui as QtGui
+
+#時計本体部
+from Model.TinyClockModel import TinyClockModel
+
+#時計表示部
+from View.mainwindow import Ui_MainWindow
+from View.TinyClockViewGUI import TinyDigitalClockView
+from View.TinyClockViewGUI import TinyAnalogClockView
+
+class ClockViewWidget(QtGui.QMainWindow, Ui_MainWindow):
+    """
+    @brief  TinyClock GUI版を表示する為のウィジェット.
+    """
+
+    def __init__(self, *args, **kw):
+        """
+        @brief  初期化.
+        """
+        #スーパークラスの初期化.
+        QtGui.QMainWindow.__init__(self, *args, **kw)
+        self.setupUi(self)
+
+        #時計本体部を生成.
+        self.ClockModel = TinyClockModel()
+
+        #デジタル時計表示部を生成.
+        self.DigitalClock = TinyDigitalClockView()
+        self.DigitalClock.setup_ui(self.dateTimeEdit)
+
+        #アナログ時計表示部を生成.
+        self.AnalogClock = TinyAnalogClockView(self.widget)
+        self.AnalogClock.setup_ui(self.dateEdit)
+
+        #時計 本体部 と 表示部 を接続
+        self.ClockModel.attach(self.DigitalClock)
+        self.ClockModel.attach(self.AnalogClock)
+
+        #タイマーを設定（時計本体部と接続. 1000mse毎に時刻を設定）
+        self.timer = QtCore.QTimer(parent=self)
+        self.timer.timeout.connect(self.ClockModel.set_time)
+        self.timer.setInterval(1*1000)
+        self.timer.start()
+
+
+def main():
+    """
+    @brief  TinyClock GUI版 main処理.
+    """
+    app = QtGui.QApplication(sys.argv)
+
+    panel = ClockViewWidget()
+
+    main_window = QtGui.QMainWindow()
+    main_window.setGeometry(0, 0, panel.width(), panel.height())
+    main_window.setWindowTitle("TinyClock")
+    main_window.setCentralWidget(panel)
+    main_window.show()
+
+    app.exec_()
+
+
+if __name__=='__main__':
+	main()
+
